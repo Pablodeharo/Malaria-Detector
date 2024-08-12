90e019f81ecaaedee4c7cf5c659b6c0bb3403a2a
Pablodeharo <pablodeharo872@gmail.com>
2024-08-12 13:08:59 +0200

    cambios

--- malaria_detection/detector/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Prediction(models.Model):
-    image = models.ImageField(upload_to='uploads/')
-    result = models.CharField(max_length=50)
-    confidence = models.FloatField()
-    date = models.DateTimeField(auto_now_add=True)
