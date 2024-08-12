04012f5658aefc4779916e12196786c7f6701e3a
Pablodeharo <pablodeharo872@gmail.com>
2024-08-12 13:52:56 +0200

    streamlit

--- src/app.py
+++ src/app.py
@@ -1,29 +1,102 @@
 import streamlit as st
-import numpy as np
+import tensorflow as tf
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
 from PIL import Image
 
-# Ruta completa al archivo del modelo
-model_path = r'C:\Users\lenovo\Desktop\Malaria Detection\Malaria Detector\src\mi_modelo.h5'
-model = load_model(model_path)
+# Load the model
+MODEL_PATH = r'C:\Users\lenovo\Desktop\Malaria Detection\Malaria Detector\src\mi_modelo.h5'
+model = load_model(MODEL_PATH)
 
-st.title('Clasificación de Imágenes de Malaria')
+# Sample images directory
+SAMPLE_IMG_DIR = r'C:\Users\lenovo\Desktop\Malaria Detection\Malaria Detector\src\image_sample'
 
-uploaded_file = st.file_uploader("Elige una imagen...", type="jpg")
+# Banner image path
+BANNER_PATH = r'C:\Users\lenovo\Desktop\Malaria Detection\Malaria Detector\src\assets\banner.jpeg'
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Imagen subida.', use_column_width=True)
-    st.write("")
-    st.write("Clasificando...")
+def preprocess_image(img):
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    return img / 255.0
 
-    # Preprocesar la imagen
-    image = image.resize((224, 224))
-    image = np.array(image)
-    image = image.reshape((1, 224, 224, 3))
+def predict(img):
+    preprocessed_img = preprocess_image(img)
+    prediction = model.predict(preprocessed_img)
+    return prediction[0][0]
 
-    # Realizar la predicción
-    prediction = model.predict(image)
-    class_idx = np.argmax(prediction, axis=1)
+def resize_image(img, max_size=(300, 300)):
+    img.thumbnail(max_size)
+    return img
 
-    st.write(f'La imagen es de la clase: {class_idx[0]}')
+# Display banner
+st.image(BANNER_PATH, use_column_width=True)
+
+# Create three columns for the buttons
+col1, col2, col3 = st.columns(3)
+
+# GitHub button
+with col1:
+    github_html = """
+    <a href="https://github.com/Pablodeharo/Phishing-Domain-Detection" target="_blank">
+        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="30" height="30">
+        GitHub
+    </a>
+    """
+    st.markdown(github_html, unsafe_allow_html=True)
+
+# LinkedIn button
+with col2:
+    linkedin_html = """
+    <a href="https://www.linkedin.com/in/pablo-de-haro-pishoudt-0871972b6/" target="_blank">
+        <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" width="30" height="30">
+        LinkedIn
+    </a>
+    """
+    st.markdown(linkedin_html, unsafe_allow_html=True)
+
+# Documentation (Notion) button
+with col3:
+    notion_html = """
+    <a href="https://tiny-citrine-a6e.notion.site/Phishing-Domain-Detection-a9c3c58fc27746b586d43352e4ebe075" target="_blank">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png" width="30" height="30">
+        Documentation
+    </a>
+    """
+    st.markdown(notion_html, unsafe_allow_html=True)
+
+st.title('Malaria Detector')
+
+# Option to choose between uploading image or using sample
+option = st.radio(
+    "Choose an option:",
+    ('Upload my own image', 'Use a sample image')
+)
+
+if option == 'Upload my own image':
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+    if uploaded_file is not None:
+        img = Image.open(uploaded_file)
+        img_display = resize_image(img.copy())
+        img = img.resize((224, 224))
+        st.image(img_display, caption='Uploaded Image', use_column_width=False)
+else:
+    # List of sample images
+    sample_images = os.listdir(SAMPLE_IMG_DIR)
+    selected_sample = st.selectbox("Choose a sample image:", sample_images)
+    img_path = os.path.join(SAMPLE_IMG_DIR, selected_sample)
+    img = Image.open(img_path)
+    img_display = resize_image(img.copy())
+    img = img.resize((224, 224))
+    st.image(img_display, caption='Selected Sample Image', use_column_width=False)
+
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    if st.button('Predict'):
+        prediction = predict(img)
+        if prediction > 0.5:
+            st.error(f'Prediction: Infected with malaria (Probability: {prediction:.2f})')
+        else:
+            st.success(f'Prediction: Not infected (Probability: {1-prediction:.2f})')
+
